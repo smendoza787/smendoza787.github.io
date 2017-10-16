4f473c1a20800c5bc045fbb3393d8db69068f9c0
Sergio Mendoza <smendoza787@gmail.com>
2017-10-15 23:27:18 -0700

    fixed slant div, changed scroll animation values

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 $(function() {
   $(document).scroll(function() {
     var $nav = $('.navbar')
-    $nav.toggleClass('scrolled', $(this).scrollTop() > ($('.welcome').height() - 275))
+    $nav.toggleClass('scrolled', $(this).scrollTop() > ($('#about').height() - 200))
   })
   $(window).scroll(function(){
     var $wScroll = $(this).scrollTop()
     $('.name').css({
-      'transform' : 'translate(0px, '+ $wScroll /2.5 +'%)'
+      'transform' : 'translate(0px, -'+ $wScroll /2.7 +'%)'
     })
     $('.title').css({
       'opacity' : Math.floor($wScroll) + '.0'
